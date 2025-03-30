5ba3949e7db077ee46beee1b31e2c5e5278b68bf
“SpindriftStar” <spindrift@mail.ustc.edu.cn>
2025-03-30 15:20:50 +0800

    add host.interface

---
+++ host/interface.py
@@ -0,0 +1,6 @@
+class Interface:
+    def __init__(self, interface_id, host_id, ip, port):
+        self.interface_id = interface_id
+        self.host_id = host_id
+        self.ip = ip
+        self.port = port
